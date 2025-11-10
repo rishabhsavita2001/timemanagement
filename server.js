beadde7b82180831fa88f3b629cb31e79691f20f
rishabhsavita2001 <rishabh738043@gmail.com>
2025-11-10 18:12:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -102,10 +102,6 @@ app.use('*', (req, res) => {
 app.use(errorHandler);
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`🚀 Working Time API Server running on port ${PORT}`);
-  console.log(`📊 Environment: ${process.env.NODE_ENV}`);
-  console.log(`🗄️  Database: ${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
-});
+
 
 module.exports = app;
